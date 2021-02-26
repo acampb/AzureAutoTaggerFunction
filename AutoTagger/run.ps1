7205fe86d59195b450fd5bf8178e7da994799ea1
Andrew Campbell <andrewbcampbell@outlook.com>
2021-02-26 08:56:22 -0500

    add additional output for troubleshooting

--- AutoTagger/run.ps1
+++ AutoTagger/run.ps1
@@ -3,7 +3,7 @@ param($eventGridEvent, $TriggerMetadata)
 # Make sure to pass hashtables to Out-String so they're logged correctly
 $eventGridEvent | Out-String | Write-Host
 
-Write-Output $eventGridEvent.data.claims
+Write-Output $eventGridEvent.data.claims | Format-List
 
 $name = $eventGridEvent.data.claims.name
 Write-Output "NAME: $name"
